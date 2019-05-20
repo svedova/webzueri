d5bb2162836691a8395668af99dc4301fb2af16e
Savas Vedova <savas@vsk.me>
2019-05-20 19:15:40 +0200

    Extract CSS

--- src/server.js
+++ src/server.js
@@ -1,27 +1,49 @@
 import React from "react";
 import { renderToString } from "react-dom/server";
+import { SheetsRegistry } from "jss";
+import { MuiThemeProvider, createGenerateClassName } from "@material-ui/core/styles"; //prettier-ignore
+import JssProvider from "react-jss/lib/JssProvider";
 import App from "./App";
 import * as actions from "./App.actions";
 import sk from "@stormkit/api";
 
+global.__STATE__ = {};
+
 export default async (req, res) => {
-  global.__STATE__ = {};
+  const state = global.__STATE__;
+
+  // Create a sheetsRegistry instance.
+  const sheetsRegistry = new SheetsRegistry();
+
+  // Create a sheetsManager instance.
+  const sheetsManager = new Map();
+
+  // Create a new class name generator.
+  const generateClassName = createGenerateClassName();
 
   await actions.fetchTalks({
     setTalks: talks => {
-      global.__STATE__.talks = talks;
+      state.talks = talks;
     }
   });
 
-  sk.log("debug", "Populated state:", global.__STATE__);
   sk.log("request", req);
 
+  const content = renderToString(
+    <JssProvider
+      registry={sheetsRegistry}
+      generateClassName={generateClassName}
+    >
+      <MuiThemeProvider sheetsManager={sheetsManager}>
+        <App />
+      </MuiThemeProvider>
+    </JssProvider>
+  );
+
+  const styles = `<style id="jss-server-side">${sheetsRegistry.toString()}</style>`;
+  const script = `<script>window.__STATE__ = ${JSON.stringify(global.__STATE__)}</script>`; // prettier-ignore
+
   return res.send({
-    body: {
-      content: renderToString(<App />),
-      state: `<script>window.__STATE__ = ${JSON.stringify(
-        global.__STATE__
-      )}</script>`
-    }
+    body: { content, styles, state: script }
   });
 };
